5cefca56a818fa0c27546ab70c3d4201eb7642b2
SIVA PRASAD I K <82268712+sprasadik2010@users.noreply.github.com>
2022-10-26 23:43:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import face_recognition
 import os
 import shutil
-
-known_image = face_recognition.load_image_file("1.png")
-known_encoding = face_recognition.face_encodings(known_image)[0]
+import uuid
 
 
 for f in os.listdir("images"):
-    try:
-        unknown_image = face_recognition.load_image_file("images\\" + f)
-        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-        results = face_recognition.compare_faces([known_encoding], unknown_encoding, 0.5)
-        if (results == [True]):
-            shutil.copy2("images\\" + f, 'SameFace')
-        else:
-            shutil.copy2("images\\" + f, 'OtherFaces')
-    except Exception as e:
-        shutil.copy2("images\\" + f, 'NoFace') # complete target filename given
-        results = e
+    foldername = str(uuid.uuid4())
+    os.makedirs(foldername)
+    known_image = face_recognition.load_image_file("images\\" + f)
+    known_encoding = face_recognition.face_encodings(known_image)[0]
+    for g in os.listdir("images"):
+        try:
+            unknown_image = face_recognition.load_image_file("images\\" + g)
+            unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+            results = face_recognition.compare_faces([known_encoding], unknown_encoding, 0.5)
+            if (results == [True]):
+                shutil.move("images\\" + g, foldername)
+            # else:
+            #     shutil.copy2("images\\" + f, 'OtherFaces')
+        except Exception as e:
+            try:
+                shutil.move("images\\" + g, 'NoFace') # complete target filename given
+            except:
+                results = e
 
-    print("{}: {}".format(f,results))
+
+        # print("{}: {}".format(f,results))
