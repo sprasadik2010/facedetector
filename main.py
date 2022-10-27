cfd97281a93e478970875c34291e28a643eb5fac
SIVA PRASAD I K <82268712+sprasadik2010@users.noreply.github.com>
2022-10-27 17:01:54 +0530

    Applying Async

--- main.py
+++ main.py
@@ -2,27 +2,33 @@ import face_recognition
 import os
 import shutil
 import uuid
+import asyncio
 
+
+
+async def finddups(img):
+    try:
+        known_image = face_recognition.load_image_file("images\\" + f)
+        known_encoding = face_recognition.face_encodings(known_image)[0]
+        foldername = str(uuid.uuid4())
+        os.makedirs(foldername)
+        for g in os.listdir("images"):
+            try:
+                unknown_image = face_recognition.load_image_file("images\\" + g)
+                unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+                results = face_recognition.compare_faces([known_encoding], unknown_encoding, 0.5)
+                if (results == [True]):
+                    shutil.move("images\\" + g, foldername)
+                # else:
+                #     shutil.copy2("images\\" + f, 'OtherFaces')
+            except Exception as e:
+                try:
+                    shutil.move("images\\" + g, 'NoFace')  # complete target filename given
+                except:
+                    results = e
+    except:
+        results = ""
 
 for f in os.listdir("images"):
-    foldername = str(uuid.uuid4())
-    os.makedirs(foldername)
-    known_image = face_recognition.load_image_file("images\\" + f)
-    known_encoding = face_recognition.face_encodings(known_image)[0]
-    for g in os.listdir("images"):
-        try:
-            unknown_image = face_recognition.load_image_file("images\\" + g)
-            unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-            results = face_recognition.compare_faces([known_encoding], unknown_encoding, 0.5)
-            if (results == [True]):
-                shutil.move("images\\" + g, foldername)
-            # else:
-            #     shutil.copy2("images\\" + f, 'OtherFaces')
-        except Exception as e:
-            try:
-                shutil.move("images\\" + g, 'NoFace') # complete target filename given
-            except:
-                results = e
+    asyncio.run(finddups(f))
 
-
-        # print("{}: {}".format(f,results))
